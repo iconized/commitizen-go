c3715275a0afd9900c68465d3fe14965cdb2334d
Abhiraj Roy <157954129+iconized@users.noreply.github.com>
2024-06-01 15:47:44 +0530

    style(messages): add emojis

--- commit/defaultConfig.go
+++ commit/defaultConfig.go
@@ -8,30 +8,30 @@ var defaultConfig = `{
 				"desc": "Select the type of change that you're committing:",
 				"form": "select",
 				"options": [
-					{ "name": "feat", "desc": "feat: A new feature" },
-					{ "name": "fix", "desc": "fix: A bug fix" },
-					{ "name": "docs", "desc": "docs: Documentation only changes" },
+					{ "name": "✨feat", "desc": "✨feat: A new feature" },
+					{ "name": "🐛fix", "desc": "🐛fix: A bug fix" },
+					{ "name": "📚docs", "desc": "📚docs: Documentation only changes" },
 					{
 					  "name": "style",
 					  "desc":
 						"style: Changes that do not affect the meaning of the code\n            (white-space, formatting, missing semi-colons, etc)"
 					},
 					{
-					  "name": "refactor",
-					  "desc": "refactor: A code change that neither fixes a bug nor adds a feature"
+					  "name": "🔨refactor",
+					  "desc": "🔨refactor: A code change that neither fixes a bug nor adds a feature"
 					},
 					{
-					  "name": "perf",
-					  "desc": "perf: A code change that improves performance"
+					  "name": "🐎perf",
+					  "desc": "🐎perf: A code change that improves performance"
 					},
-					{ "name": "test", "desc": "test: Adding missing tests" },
+					{ "name": "🚨test", "desc": "🚨test: Adding missing tests" },
 					{
-					  "name": "chore",
+					  "name": "♿chore",
 					  "desc":
-						"chore: Changes to the build process or auxiliary tools\n            and libraries such as documentation generation"
+						"♿chore: Changes to the build process or auxiliary tools\n            and libraries such as documentation generation"
 					},
-					{ "name": "revert", "desc": "revert: Revert to a commit" },
-					{ "name": "WIP", "desc": "WIP: Work in progress" }
+					{ "name": "⏪revert", "desc": "⏪revert: Revert to a commit" },
+					{ "name": "🚧WIP", "desc": "🚧WIP: Work in progress" }
 				],
 				"required": true
 			},
